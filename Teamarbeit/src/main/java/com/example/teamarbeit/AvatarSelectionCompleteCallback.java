e64ef6f98222b156c0a1c7645f0bc2c0af067e91
sabhyeta1 <sabhyeta@gmail.com>
2024-01-21 15:17:47 +0100

    comments + sources added to Avatars.java as well as AvatarSelectionCompleteCallback.java

--- Teamarbeit/src/main/java/com/example/teamarbeit/AvatarSelectionCompleteCallback.java
+++ Teamarbeit/src/main/java/com/example/teamarbeit/AvatarSelectionCompleteCallback.java
@@ -1,7 +1,15 @@
 package com.example.teamarbeit;
 
+/**
+ * The following 4 lines of Code have been inspired by the Internet| https://stackoverflow.com/questions/19405421/what-is-a-callback-method-in-java-term-seems-to-be-used-loosely,last visit: 20.01.2024
+ */
+
 import javafx.scene.control.Button;
+// imports Button class
 
 public interface AvatarSelectionCompleteCallback {
     void onSelectionComplete(Button backButton);
+    //onSelectionComplete = method that does not return any value --> void
+    //called after avatar has been selected
+    //backButton as parameter; able to switch back to main Menu after the avatar selection is complete.
 }
